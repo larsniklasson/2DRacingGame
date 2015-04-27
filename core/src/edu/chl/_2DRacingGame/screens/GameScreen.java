5ee7a3dc192257ab18424c841046b23cc265efbd
vchristoffersson <vchristoffersson@gmail.com>
2015-04-27 22:05:28 +0200

    countdown at start (graphics update later)

--- core/src/edu/chl/_2DRacingGame/screens/GameScreen.java
+++ core/src/edu/chl/_2DRacingGame/screens/GameScreen.java
@@ -1,6 +1,9 @@
 package edu.chl._2DRacingGame.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import edu.chl._2DRacingGame.world.GameRenderer;
 import edu.chl._2DRacingGame.world.GameWorld;
 
@@ -11,17 +14,42 @@ public class GameScreen implements Screen {
 
     private GameWorld world;
     private GameRenderer renderer;
+    private Boolean gameStart;
+
+    private float elapsedTime;
+    private long startTime;
+    private int centerWidth;
+    private int centerHeight;
+
+    private Texture ctdwnOne;
+    private Texture ctdwnTwo;
+    private Texture ctdwnThree;
+    private Texture ctdwnGo;
+
+    private SpriteBatch spriteBatch;
 
     public GameScreen(){
         world = new GameWorld();
         renderer = new GameRenderer(world);
 
+        gameStart = true;
 
+        ctdwnOne = new Texture("..\\assets\\one.png");
+        ctdwnTwo = new Texture("..\\assets\\two.png");
+        ctdwnThree = new Texture("..\\assets\\three.png");
+        ctdwnGo = new Texture("..\\assets\\go.png");
+
+        centerWidth = Gdx.graphics.getWidth()/2;
+        centerHeight = Gdx.graphics.getHeight()/2;
+
+        spriteBatch = new SpriteBatch();
 
     }
 
     @Override
     public void show() {
+
+        startTime = System.nanoTime();
 
     }
 
@@ -31,6 +59,24 @@ public class GameScreen implements Screen {
         world.update(delta);
         renderer.render();
 
+
+        if(gameStart) {
+            elapsedTime = (System.nanoTime() - startTime) / 1000000000.0f;
+            spriteBatch.begin();
+
+            if (elapsedTime < 1f) {
+                spriteBatch.draw(ctdwnThree, centerWidth, centerHeight);
+            } else if (elapsedTime < 2f) {
+                spriteBatch.draw(ctdwnTwo, centerWidth, centerHeight);
+            } else if (elapsedTime < 3f) {
+                spriteBatch.draw(ctdwnOne, centerWidth, centerHeight);
+            } else if (elapsedTime < 4f) {
+                spriteBatch.draw(ctdwnGo, centerWidth, centerHeight);
+            } else if (elapsedTime < 5f) {
+                gameStart = false;
+            }
+            spriteBatch.end();
+        }
     }
 
     @Override
