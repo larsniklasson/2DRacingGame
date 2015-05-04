f5c427143bf667974b682c4833ff43173f8e396d
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-05 00:39:38 +0200

    Room joining/creating now works as intented

--- core/src/edu/chl/_2DRacingGame/controllers/MultiplayerSetupController.java
+++ core/src/edu/chl/_2DRacingGame/controllers/MultiplayerSetupController.java
@@ -1,14 +1,15 @@
 package edu.chl._2DRacingGame.controllers;
 
+import com.badlogic.gdx.Gdx;
 import com.shephertz.app42.gaming.multiplayer.client.WarpClient;
 import com.shephertz.app42.gaming.multiplayer.client.command.WarpResponseResultCode;
-import com.shephertz.app42.gaming.multiplayer.client.events.ConnectEvent;
-import com.shephertz.app42.gaming.multiplayer.client.events.LiveRoomInfoEvent;
-import com.shephertz.app42.gaming.multiplayer.client.events.RoomEvent;
+import com.shephertz.app42.gaming.multiplayer.client.events.*;
 import com.shephertz.app42.gaming.multiplayer.client.listener.ConnectionRequestListener;
 import com.shephertz.app42.gaming.multiplayer.client.listener.RoomRequestListener;
+import com.shephertz.app42.gaming.multiplayer.client.listener.ZoneRequestListener;
 
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
@@ -16,7 +17,7 @@ import java.util.Objects;
 /**
  * @author Daniel Sunnerberg
  */
-public class MultiplayerSetupController implements RoomRequestListener {
+public class MultiplayerSetupController implements RoomRequestListener, ZoneRequestListener {
 
     private static final String API_KEY = "1b1136c934963a62964ccdd973e52b476f3977a743451d54c4f5d427d573a517";
     private static final String SECRET_KEY = "a641f46a9b4ce012d502ae86d235de8aa5445c8fa6d16fd76b9ea0d494ea1327";
@@ -29,11 +30,11 @@ public class MultiplayerSetupController implements RoomRequestListener {
             @Override
             public void onConnectDone(ConnectEvent connectEvent) {
                 if (connectEvent.getResult() == WarpResponseResultCode.SUCCESS) {
+                    Gdx.app.log("MultiplayerSetupController", "Successfully connected to AppWarp-servers.");
                     joinRoom();
                 } else {
-                    System.out.println(connectEvent.getResult());
+                    Gdx.app.log("MultiplayerSetupController", "Failed to connect to AppWarp: " + connectEvent.getResult());
                     // TODO not available
-                    System.out.println("An error occured, could not connect.");
                 }
             }
 
@@ -45,12 +46,13 @@ public class MultiplayerSetupController implements RoomRequestListener {
             public void onInitUDPDone(byte b) {
             }
         });
+        warpClient.addZoneRequestListener(this);
         warpClient.addRoomRequestListener(this);
     }
 
     private void joinRoom() {
         warpClient.initUDP();
-        warpClient.joinRoomInRange(1, 2, false);
+        warpClient.joinRoomInRange(1, 1, false);
     }
 
     private WarpClient getWarpInstance() {
@@ -68,7 +70,6 @@ public class MultiplayerSetupController implements RoomRequestListener {
     }
 
     public void connect() {
-        System.out.println(getUserName());
         warpClient.connectWithUserName(getUserName());
     }
 
@@ -87,27 +88,56 @@ public class MultiplayerSetupController implements RoomRequestListener {
                 break;
             default:
                 warpClient.disconnect();
-                System.out.println("Failed to join room."); // TODO
+                // TODO
+                Gdx.app.log("MultiplayerSetupController", "Failed to find a suitable room. Disconnecting.");
                 break;
         }
     }
 
     private void successfullyJoinedRoom(String roomId) {
-        System.out.println("Found room!");
+        Gdx.app.log("MultiplayerSetupController", "Joined room. Subscribing to it.");
         warpClient.subscribeRoom(roomId);
     }
 
     private void createEmptyRoom() {
-        System.out.println("Didn't find a room; creating one instead!");
+        Gdx.app.log("MultiplayerSetupController", "Found no room, creating a new one.");
         HashMap<String, Object> data = new HashMap<>();
         data.put("result", "");
         String userName = getUserName();
-        warpClient.createRoom(userName, userName, 2, data);
-        warpClient.getLiveRoomInfo(userName);
+        // Create the room. The listener "onCreateRoomDone" will then subscribe to the room
+        warpClient.createRoom("quickrace-" + userName, userName, 2, data);
     }
 
+    @Override
+    public void onCreateRoomDone(RoomEvent roomEvent) {
+        warpClient.joinRoom(roomEvent.getData().getId());
+    }
 
     // Empty methods to satisfy required interfaces.
+    @Override
+    public void onDeleteRoomDone(RoomEvent roomEvent) {
+    }
+
+    @Override
+    public void onGetAllRoomsDone(AllRoomsEvent allRoomsEvent) {
+    }
+
+    @Override
+    public void onGetOnlineUsersDone(AllUsersEvent allUsersEvent) {
+    }
+
+    @Override
+    public void onGetLiveUserInfoDone(LiveUserInfoEvent liveUserInfoEvent) {
+    }
+
+    @Override
+    public void onSetCustomUserDataDone(LiveUserInfoEvent liveUserInfoEvent) {
+    }
+
+    @Override
+    public void onGetMatchedRoomsDone(MatchedRoomsEvent matchedRoomsEvent) {
+    }
+
     @Override
     public void onSubscribeRoomDone(RoomEvent roomEvent) {
     }
