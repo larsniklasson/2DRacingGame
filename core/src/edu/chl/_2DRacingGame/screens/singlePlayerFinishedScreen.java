09cfbe1811b763f11bc60fabef2e51b6721813e7
Billthekid <ingvarsson.anton@gmail.com>
2015-05-12 15:20:44 +0200

    Start of SinglePlayerFinishedScreen

---
+++ core/src/edu/chl/_2DRacingGame/screens/singlePlayerFinishedScreen.java
@@ -0,0 +1,47 @@
+package edu.chl._2DRacingGame.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Anton on 2015-05-12.
+ */
+public class singlePlayerFinishedScreen implements Screen {
+
+    public singlePlayerFinishedScreen() {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
