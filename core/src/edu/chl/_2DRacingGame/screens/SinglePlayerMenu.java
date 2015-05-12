e86f37c7be061e50df3c5ae46cdd9ce64e8be270
Billthekid <ingvarsson.anton@gmail.com>
2015-05-12 09:49:00 +0200

    fix to dropdown in singlePlayerMenu

--- core/src/edu/chl/_2DRacingGame/screens/SinglePlayerMenu.java
+++ core/src/edu/chl/_2DRacingGame/screens/SinglePlayerMenu.java
@@ -116,6 +116,7 @@ public class SinglePlayerMenu implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
         stage.draw();
     }
 
