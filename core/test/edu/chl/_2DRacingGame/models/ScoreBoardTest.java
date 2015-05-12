2216843408ae34d5a1a7ebd80bbfe8511fa393fd
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-12 16:27:04 +0200

    Added JUnit support and configured Gradle accordingly

---
+++ core/test/edu/chl/_2DRacingGame/models/ScoreBoardTest.java
@@ -0,0 +1,16 @@
+package edu.chl._2DRacingGame.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Daniel Sunnerberg
+ */
+public class ScoreBoardTest {
+
+    @Test
+    public void foo() {
+        assertEquals(1, 1);
+    }
+}
