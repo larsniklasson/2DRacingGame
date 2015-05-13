2774dfe197359f4551cb7d8b88da8802487ea025
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-13 17:09:38 +0200

    Added unit tests for Checkpoint model

---
+++ core/test/edu/chl/_2DRacingGame/models/CheckpointTest.java
@@ -0,0 +1,38 @@
+package edu.chl._2DRacingGame.models;
+
+import com.badlogic.gdx.math.Vector2;
+import jdk.internal.org.objectweb.asm.util.CheckSignatureAdapter;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Daniel Sunnerberg
+ */
+public class CheckpointTest {
+
+    @Test
+    public void testIsValidEntry() throws Exception {
+        final Vector2 begin = new Vector2(0, 0);
+        Vector2 end = new Vector2(10, 10);
+
+        Checkpoint checkpoint = new Checkpoint();
+        assertFalse("Without any valid directions all should fail", checkpoint.isValidEntry(begin, end));
+ 
+        checkpoint.addAllowedPassingDirection(CheckpointDirection.EAST);
+        assertFalse("When only allowing EAST, passing from southwest should not be allowed", checkpoint.isValidEntry(begin, end));
+
+        checkpoint = new Checkpoint();
+        checkpoint.addAllowedPassingDirection(CheckpointDirection.WEST);
+        assertTrue("Passing from southwest should be allowed if only allowing west", checkpoint.isValidEntry(begin, end));
+
+        checkpoint.addAllowedPassingDirection(CheckpointDirection.SOUTH);
+        assertTrue("Passing from southwest should be allowed if allowing southwest", checkpoint.isValidEntry(begin, end));
+    }
+
+    @Test
+    public void testGetType() throws Exception {
+        assertEquals(CheckpointType.LAP_END, new Checkpoint(CheckpointType.LAP_END).getType());
+        assertEquals(CheckpointType.INVISIBLE, new Checkpoint().getType());
+    }
+}
