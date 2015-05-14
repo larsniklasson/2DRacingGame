752bd71eabc8ce5e3641f24601bdba74f4bbeadc
Billthekid <ingvarsson.anton@gmail.com>
2015-05-14 11:14:57 +0200

    Clean the code of MainMenu

--- core/src/edu/chl/_2DRacingGame/screens/MainMenu.java
+++ core/src/edu/chl/_2DRacingGame/screens/MainMenu.java
@@ -13,43 +13,24 @@ import edu.chl._2DRacingGame._2DRacingGame;
 /**
  *
  */
-public class MainMenu implements Screen {
+public class MainMenu extends GUIScreen {
 
     private _2DRacingGame game;
-    private SpriteBatch batch;
-    private Skin skin;
-    private Stage stage;
     private Table table;
 
     public MainMenu(_2DRacingGame game) {
-        this.game = game;
-        batch = new SpriteBatch();
-        skin = new Skin(Gdx.files.internal("uiskin.json"));
-        stage = new Stage();
-        table = new Table();
 
+        this.game = game;
+        table = new Table();
+    }
+
+    @Override
+    public void show() {
         final TextButton singlePlayer = new TextButton("Single Player", skin, "default");
         final TextButton multiPlayer = new TextButton("MultiPlayer", skin, "default");
         final TextButton options = new TextButton("options", skin, "default");
         final TextButton exit = new TextButton("Exit", skin, "default");
-
-        singlePlayer.setWidth(400f);
-        singlePlayer.setHeight(40f);
-        singlePlayer.setPosition(Gdx.graphics.getWidth() / 6, Gdx.graphics.getHeight() / 2 + 50f);
-
-        multiPlayer.setWidth(400f);
-        multiPlayer.setHeight(40f);
-        multiPlayer.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2 + 50f);
-
-        options.setWidth(150f);
-        options.setHeight(30f);
-        options.setPosition(Gdx.graphics.getWidth() / 2 - options.getWidth() / 2, Gdx.graphics.getHeight() - 400);
-
-        exit.setWidth(150f);
-        exit.setHeight(30f);
-        exit.setPosition(Gdx.graphics.getWidth() / 2 - options.getWidth() / 2, Gdx.graphics.getHeight() - 450);
-
-        Label gameTitle = new Label("2D Racing Game!", skin, "arial40");
+        final Label gameTitle = new Label("2D Racing Game!", skin, "arial40");
 
         //Adds Listeners to all buttons
         singlePlayer.addListener(new ClickListener() {
@@ -94,10 +75,6 @@ public class MainMenu implements Screen {
         table.debug(); // ger markeringar, ska tas bort
         stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
-    }
-
-    @Override
-    public void show() {
 
     }
 
@@ -111,28 +88,12 @@ public class MainMenu implements Screen {
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
     public void hide() {
         dispose();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         skin.dispose();
         stage.dispose();
     }
