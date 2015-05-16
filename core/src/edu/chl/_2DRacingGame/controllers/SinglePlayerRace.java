c30c1e600fcba57fb8a1d07bdaf7049879305b5b
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-17 01:53:41 +0200

    Missed to remove relic from previous extraction, singleplayer now works as intended

--- core/src/edu/chl/_2DRacingGame/controllers/SinglePlayerRace.java
+++ core/src/edu/chl/_2DRacingGame/controllers/SinglePlayerRace.java
@@ -1,15 +1,12 @@
 package edu.chl._2DRacingGame.controllers;
 
 import com.badlogic.gdx.Gdx;
-import edu.chl._2DRacingGame._2DRacingGame;
 import edu.chl._2DRacingGame.gameModes.TimeTrial;
 import edu.chl._2DRacingGame.gameObjects.Car;
 import edu.chl._2DRacingGame.gameObjects.Vehicle;
 import edu.chl._2DRacingGame.models.GameMap;
 import edu.chl._2DRacingGame.models.MapScores;
 import edu.chl._2DRacingGame.models.MapScoresPersistor;
-import edu.chl._2DRacingGame.screens.GameScreen;
-import edu.chl._2DRacingGame.world.GameWorld;
 
 /**
  * @author Daniel Sunnerberg
@@ -18,7 +15,6 @@ import edu.chl._2DRacingGame.world.GameWorld;
 public class SinglePlayerRace extends RaceController {
 
     private MapScoresPersistor scoresPersistor;
-    private GameWorld world;
 
     public SinglePlayerRace(GameController gameController) {
         super(gameController);
@@ -31,13 +27,13 @@ public class SinglePlayerRace extends RaceController {
         scoresPersistor = new MapScoresPersistor(getMap(), getMode());
         scoresPersistor.findInstance();
 
-        Vehicle vehicle = new Car(world.getb2World());
+        Vehicle vehicle = new Car(getWorld().getb2World());
         getPlayer().setVehicle(vehicle);
     }
 
     private void startRace() {
-        world.addPlayer(getPlayer());
-        world.spawnPlayers();
+        getWorld().addPlayer(getPlayer());
+        getWorld().spawnPlayers();
         gameController.setScreen(getScreen());
     }
 
@@ -78,7 +74,6 @@ public class SinglePlayerRace extends RaceController {
     @Override
     public void dispose() {
         super.dispose();
-        world.dispose();
     }
 
 }
