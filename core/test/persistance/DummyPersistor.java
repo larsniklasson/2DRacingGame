13ed83114df58220e906316626ce46d625c6eb76
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-20 02:50:23 +0200

    Added dummy persistor to be able to test persistance

---
+++ core/test/persistance/DummyPersistor.java
@@ -0,0 +1,34 @@
+package persistance;
+
+import edu.chl._2DRacingGame.persistance.Persistor;
+import edu.chl._2DRacingGame.persistance.PersistorException;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple persistor imitator which stores the instances to a map to mimick persistance.
+ * Used for testing purposes.
+ *
+ * @author Daniel Sunnerberg
+ */
+public class DummyPersistor<T> implements Persistor<T> {
+
+    private final Map<String, T> storage = new HashMap<>();
+
+    @Override
+    public void persist(T instance, String persistKey) {
+        storage.put(persistKey, instance);
+    }
+
+    @Override
+    public T getPersistedInstance(String persistKey, Type originType) throws PersistorException {
+        if (! storage.containsKey(persistKey)) {
+            throw new PersistorException("No persisted instance found");
+        }
+
+        return storage.get(persistKey);
+    }
+
+}
