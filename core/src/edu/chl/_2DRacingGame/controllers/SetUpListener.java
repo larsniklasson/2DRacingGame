ff359937a6450943409336a31017eab173deb7d8
Billthekid <ingvarsson.anton@gmail.com>
2015-05-23 23:24:18 +0200

    Rename SetUpListener

--- core/src/edu/chl/_2DRacingGame/controllers/SetUpListener.java
+++ core/src/edu/chl/_2DRacingGame/controllers/SetUpListener.java
@@ -3,6 +3,7 @@ package edu.chl._2DRacingGame.controllers;
 /**
  * Created by Anton on 2015-05-20.
  */
-public interface setUpListener {
+public interface SetUpListener {
     void setUpRace(String vehicleType, String mapName,String difficulty,  int laps, int opponents);
+    void displayMainMenu();
 }
