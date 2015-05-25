bb4f1923e12e5558b069e3c689f20080a39d0afb
Billthekid <ingvarsson.anton@gmail.com>
2015-05-25 21:53:45 +0200

    Changed the pausemenu to use window instead of table

--- core/src/edu/chl/_2DRacingGame/screens/PauseScreen.java
+++ core/src/edu/chl/_2DRacingGame/screens/PauseScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import edu.chl._2DRacingGame.Assets;
@@ -19,6 +20,7 @@ import edu.chl._2DRacingGame.Assets;
 public class PauseScreen extends GUIScreen {
 
     private Table table;
+    private Window window;
 
     public PauseScreen() {
         show();
@@ -61,9 +63,8 @@ public class PauseScreen extends GUIScreen {
         });
 
         table = new Table(skin);
+        window = new Window("", skin);
         table.add(pausedLabel);
-        //table.background(new TextureRegionDrawable(new TextureRegion((Assets.dirt))));
-        //TODO hitta en bild som backgrund
         table.row();
         table.add(resumeButton).padTop(50).width(200f).height(30f);
         table.row();
@@ -73,14 +74,18 @@ public class PauseScreen extends GUIScreen {
         table.row();
         table.add(quitRaceButton).padTop(50).width(200f).height(30f);
         table.row();
-        table.setBounds(0 ,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         table.debug();
-        stage.addActor(table);
-        Gdx.input.setInputProcessor(stage);
+        window.setResizable(false);
+        window.setMovable(false);
+        window.setSize(table.getWidth()/3f, table.getHeight()/1.5f);
+        window.setPosition(stage.getWidth() / 2 - window.getWidth() / 2 ,stage.getHeight() / 2 - window.getWidth() / 2);
+
+        window.add(table);
     }
 
-    public Table getPausedScreen(){
-        return table;
+    public Window getPausedScreen(){
+        return window;
     }
 
     @Override
