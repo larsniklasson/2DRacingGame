ed3c22595e2c6551d78bc3295538c182204a0b73
Billthekid <ingvarsson.anton@gmail.com>
2015-05-26 19:11:44 +0200

    Removed a lot of useless code from GameScreen

--- core/src/edu/chl/_2DRacingGame/screens/GameScreen.java
+++ core/src/edu/chl/_2DRacingGame/screens/GameScreen.java
@@ -35,22 +35,13 @@ public class GameScreen extends GUIScreen implements Screen {
     private Texture ctdwnThree;
     private Texture ctdwnGo;
 
-    private enum State {GAME_RUNNING, GAME_PAUSED}
-    private State state;
     private SpriteBatch spriteBatch;
-    private  Vector3 touchPoint;
-    private Rectangle resumeBounds;
-    private Rectangle exitBounds;
 
     public GameScreen(GameWorld world, GameMode mode) {
         this.world = world;
         this.gameMode = mode;
         renderer = new GameRenderer(world);
 
-        state = State.GAME_RUNNING;
-        touchPoint = new Vector3();
-        resumeBounds = new Rectangle(470,140,410,90);
-        exitBounds = new Rectangle(470,500,410,90);
         gameStart = true;
 
         ctdwnOne = new Texture("one.png");
@@ -62,84 +53,6 @@ public class GameScreen extends GUIScreen implements Screen {
         centerHeight = Gdx.graphics.getHeight()/2;
 
         spriteBatch = new SpriteBatch();
-    }
-
-    public void update(float delta){
-        switch(state){
-            case GAME_RUNNING:
-                updateRunning(delta);
-                break;
-            case GAME_PAUSED:
-                updatePaused();
-                break;
-            default: System.out.println("Something went wrong!");
-
-        }
-
-    }
-
-    private void updatePaused() {
-        if (Gdx.input.justTouched()) {
-            touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            if (resumeBounds.contains(touchPoint.x, touchPoint.y)) {
-                this.state = State.GAME_RUNNING;
-                gameMode.resume();
-                return;
-            }
-            if(exitBounds.contains(touchPoint.x, touchPoint.y)){
-                Gdx.app.exit();
-                return;
-            }
-        }
-
-        /* bortplockad för att programmet läser in ett knaptryck väldigt snabbt vilket gör att den
-           byter mellen pause och run flera gånger utan någon precision vart den slutar.
-        if (Gdx.input.isKeyPressed(Input.Keys.P)){
-            if(this.state == State.GAME_PAUSED)
-                this.state = State.GAME_RUNNING;
-
-        }*/
-    }
-
-    private void updateRunning(float delta) {
-        if (Gdx.input.isKeyPressed(Input.Keys.P)){
-            if(this.state == State.GAME_RUNNING) {
-                gameMode.pause();
-                this.state = State.GAME_PAUSED;
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE))
-            Assets.carHorn();
-
-        world.update(delta);
-    }
-
-    public void draw(float delta) {
-
-        spriteBatch.begin();
-        switch (state) {
-            case GAME_RUNNING:
-                drawRunning(delta);
-                break;
-            case GAME_PAUSED:
-                drawPaused();
-                break;
-            default:
-                System.out.println("Something went wrong!");
-        }
-        spriteBatch.end();
-    }
-
-    private void drawPaused() {
-        //stage.addActor(new PauseScreen().getPausedScreen());
-        //stage.draw();
-        //spriteBatch.draw(Assets.pauseMenu,centerWidth-Assets.pauseMenu.getWidth()/2 ,0);
-    }
-
-    private void drawRunning(float delta) {
-        renderer.render();
-        renderer.draw();
-        renderer.act(delta);
     }
 
     public void addScreenTexts(List<ScreenText> texts) {
@@ -159,7 +72,9 @@ public class GameScreen extends GUIScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        draw(delta);
+        renderer.render();
+        renderer.draw();
+        renderer.act(delta);
 
         if(gameStart) {
             elapsedTime = (System.nanoTime() - startTime) / 1000000000.0f;
@@ -179,7 +94,7 @@ public class GameScreen extends GUIScreen implements Screen {
             }
             spriteBatch.end();
         } else {
-            update(delta);
+            world.update(delta);
         }
 
     }
