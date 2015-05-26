efeace043cc370c742742db858f9a9d75347959a
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-05-26 21:42:00 +0200

    Improved spelling

--- core/src/edu/chl/_2DRacingGame/controllers/MultiplayerWorldSyncer.java
+++ core/src/edu/chl/_2DRacingGame/controllers/MultiplayerWorldSyncer.java
@@ -42,10 +42,10 @@ public class MultiplayerWorldSyncer implements UpdateListener, RaceListener {
     private static final int MIN_UPDATE_WAIT = 100;
 
     /**
-     * Assumed lancety in ms between client and server.
+     * Assumed latency in ms between client and server.
      * TODO estimate this on the fly for a more individual and realistic approximation
      */
-    private static final int ASSUMED_LANCETY = 30;
+    private static final int ASSUMED_LATENCY = 30;
 
     /**
      * The time when the last update was sent.
@@ -176,7 +176,7 @@ public class MultiplayerWorldSyncer implements UpdateListener, RaceListener {
             angle -= 2*Math.PI;
         }
 
-        float animationDuration = (MIN_UPDATE_WAIT + ASSUMED_LANCETY) / 1000f;
+        float animationDuration = (MIN_UPDATE_WAIT + ASSUMED_LATENCY) / 1000f;
         Action moveAction = Actions.moveTo(x, y, animationDuration);
 
         Action rotateAction = Actions.rotateTo(angle, animationDuration);
